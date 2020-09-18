bfe36c36f3aa1b96afb4ea8ec95546320fa55c80
milad <link.mjow@gmail.com>
2020-09-18 08:12:24 +0330

    base config of WarehouseService

---
+++ warehouse-service/src/main/java/com/productservice/WarehouseService.java
@@ -0,0 +1,14 @@
+package com.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseService.class, args);
+    }
+
+
+}
